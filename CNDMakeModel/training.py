f9dbb1fed284bb31dfaee6410ca5a652a5f98e35
ElCammel <lamygregoire1@gmail.com>
2019-07-14 15:19:10 +0200

    finished project

---
+++ CNDMakeModel/training.py
@@ -0,0 +1,153 @@
+import cv2  # working with, mainly resizing, images
+import matplotlib.pyplot as plt
+from tensorflow.python.keras.optimizers import Adam
+import numpy as np  # dealing with arrays
+from tensorflow.python.keras.layers.convolutional import Conv2D, MaxPooling2D
+from tensorflow.python.keras.layers.core import Dense, Dropout, Flatten, Activation
+from tensorflow.python.keras.models import Sequential, load_model
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+import os, random
+
+IMG_PATH = 'C:\\Users\\xlmgr\\Desktop\\Docs\\Cours\\Dataset\\'
+IMG_SIZE = 75
+EPOCHS = 80
+BS = 32
+CLASSES = 1
+INIT_LR = 1e-3
+
+train_data = []
+train_labels = []
+test_data = []
+
+MODEL_NAME = 'C:\\Users\\xlmgr\\PycharmProjects\\CNDMakeModel\\CatzNDogz.model'
+
+def get_data():
+    train_dir = IMG_PATH + 'train'
+    test_dir = IMG_PATH + 'test'
+
+    train_dogs = [(train_dir + '/{}').format(i) for i in os.listdir(train_dir) if 'dog' in i]
+    train_cats = [(train_dir + '/{}').format(i) for i in os.listdir(train_dir) if 'cat' in i]
+
+    test_images = [(test_dir + '/{}').format(i) for i in os.listdir(test_dir)]
+
+    train_images = train_dogs[:4000] + train_cats[:4000]
+    random.shuffle(train_images)
+    random.shuffle(test_images)
+
+    create_data(train_images, test_images)
+
+def create_data(train_images, test_images):
+
+    for image in test_images:
+        test_data.append(cv2.resize(cv2.imread(image, cv2.IMREAD_COLOR), (IMG_SIZE, IMG_SIZE)))
+
+    for img in train_images:
+        train_data.append(cv2.resize(cv2.imread(img, cv2.IMREAD_COLOR), (IMG_SIZE, IMG_SIZE)))
+
+        if 'dog' in img:
+            train_labels.append(1)
+        elif 'cat' in img:
+            train_labels.append(0)
+
+    np.save('train_data.npy', np.array(train_data))
+    np.save('train_labels.npy', np.array(train_labels))
+    np.save('test_images.npy', np.array(test_data))
+
+
+def make_model():
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), input_shape=(IMG_SIZE, IMG_SIZE, 3)))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(3, 3)))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(3, 3)))
+
+    model.add(Conv2D(128, (3, 3)))
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(3, 3)))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation("relu"))
+
+    model.add(Dense(CLASSES))
+    model.add(Activation("sigmoid"))
+
+    opt = Adam(lr=INIT_LR)
+    model.compile(loss="binary_crossentropy", optimizer=opt,
+                  metrics=["accuracy"])
+
+    return model
+
+
+def train_model():
+    (train_x, test_x, train_y, test_y) = train_test_split(train_data, train_labels, test_size=0.20, random_state=2)
+
+    train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2, shear_range=0.2,
+                             zoom_range=0.2, horizontal_flip=True)
+
+    val_datagen = ImageDataGenerator(rescale=1./255)
+
+    model = make_model()
+
+    H = model.fit_generator(train_datagen.flow(train_x, train_y, batch_size=BS),
+                  validation_data=val_datagen.flow(test_x, test_y, batch_size=BS), steps_per_epoch=len(train_x),
+                  epochs=EPOCHS, verbose=1)
+
+    model.save(MODEL_NAME)
+
+    plt.style.use("ggplot")
+    plt.figure()
+    N = EPOCHS
+    plt.plot(np.arange(0, N), H.history["loss"], label="train_loss")
+    plt.plot(np.arange(0, N), H.history["val_loss"], label="val_loss")
+    plt.plot(np.arange(0, N), H.history["acc"], label="train_acc")
+    plt.plot(np.arange(0, N), H.history["val_acc"], label="val_acc")
+    plt.title("Training Loss and Accuracy on Cat/Dog")
+    plt.xlabel("Epoch #")
+    plt.ylabel("Loss/Accuracy")
+    plt.legend(loc="lower left")
+    plt.savefig("plot")
+
+
+def predict():
+    x_test = np.load('test_images.npy')[0:10]
+
+    model = load_model(MODEL_NAME)
+    test_datagen = ImageDataGenerator(rescale=1.0/255)
+    i=0
+    text_labels = []
+
+    plt.figure(figsize=(75, 75))
+
+    for batch in test_datagen.flow(x_test, batch_size=1):
+        pred = model.predict(batch)
+
+        if pred > 0.5:
+            text_labels.append('dog')
+        else:
+            text_labels.append('cat')
+
+        plt.subplot(4, 4, i +1)
+        plt.title('This is a ' + text_labels[i])
+        imgplot = plt.imshow(batch[0])
+        i += 1
+
+        if i % 10 == 0:
+            break
+
+    plt.show()
+
+
+#get_data()
+
+train_data = np.load('train_data.npy')
+train_labels = np.load('train_labels.npy')
+
+#train_model()
+
+predict()
--- CatzNDogz/CNDApp/views.py
+++ CatzNDogz/CNDApp/views.py
@@ -1,19 +1,87 @@
-from flask import Flask, request, render_template
+import cv2
+import numpy as np  # dealing with arrays
+import os
+from PIL import Image
+from flask import Flask, request, render_template, send_from_directory, flash  # working with, mainly resizing, images
+from tensorflow.python.keras.models import load_model
+from tensorflow.python.keras.preprocessing.image import img_to_array
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'jfif'])
+IMG_SIZE = 75
+MODEL_NAME = 'CatzNDogz.model'
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
 
 app = Flask(__name__)
-
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Config options - Make sure you created a 'config.py' file.
 app.config.from_object('config')
 
-pet_name = 'bob'
-# To get one variable, tape app.config['MY_VARIABLE']
 
 @app.route('/')
 def index():
-    if request.method == 'GET':
-        return render_template('index.html')
+    return render_template('index.html')
+
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
     if request.method == 'POST':
-        return render_template('result.html', pet=pet_name)
+        target = os.path.join(APP_ROOT, 'static/')
+
+        if not os.path.isdir(target):
+            os.mkdir(target)
+
+        if not request.files['file']:
+            flash('No file part')
+            return render_template('index.html')
+
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            pet_name = predict(file)
+            file.stream.seek(0)
+
+            img = Image.open(file)
+            wpercent = (150 / float(img.size[0]))
+            hsize = int((float(img.size[0] * float(wpercent))))
+            img = img.resize((150, hsize), Image.ANTIALIAS)
+            destination = "/".join([target, file.filename])
+            img.save(destination)
+
+            return render_template('result.html', pet=pet_name, image_name=file.filename)
+
+@app.route('/send_image/<filename>')
+def send_image(filename):
+    return send_from_directory("static", filename)
+
+
+def predict(file):
+    image = dataFromImage(file)
+    model = load_model(MODEL_NAME)
+    label = ''
+    
+    result = model.predict(image)
+    
+    if result > 0.5:
+        label = 'dog'
+    else:
+        label = 'cat'
+
+    return label
+
+
+def dataFromImage(file):
+    data = file.read()
+    image = np.asarray(bytearray(data))
+
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    image = cv2.resize(image, (IMG_SIZE, IMG_SIZE))
+    image = img_to_array(image)
+    image = np.expand_dims(image, axis=0)
+
+    return image
 
 
 if __name__ == "__main__":
